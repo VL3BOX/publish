45dfc25aed6998864513c2ab37fa507bbf274257
iRuxu <rx6@qq.com>
2021-08-21 13:00:04 +0800

    opt notice publish

--- src/post/notice.vue
+++ src/post/notice.vue
@@ -109,7 +109,7 @@ export default {
                 // 标题
                 post_title: "",
                 // 子类型：心法、副本名等
-                post_subtype: "",
+                post_subtype: "6",
                 // 自定义字段
                 post_meta: {},
                 // 内容
