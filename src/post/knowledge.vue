c8070c3ebe0bf5151b2e866a92d64fd3f6dbc769
iRuxu <rx6@qq.com>
2022-03-01 21:33:53 +0800

    fix:knowledge clients

--- src/post/knowledge.vue
+++ src/post/knowledge.vue
@@ -7,11 +7,11 @@
 
         <el-form class="m-publish-post">
             <div class="m-publish-source">
-            <el-divider content-position="left">选择通识 *</el-divider>
-            <el-radio-group class="m-publish-action" v-model="action">
-                <el-radio-button label="new">新建词条</el-radio-button>
-                <el-radio-button label="update">维护已有词条</el-radio-button>
-            </el-radio-group>
+                <el-divider content-position="left">选择通识 *</el-divider>
+                <el-radio-group class="m-publish-action" v-model="action">
+                    <el-radio-button label="new">新建词条</el-radio-button>
+                    <el-radio-button label="update">维护已有词条</el-radio-button>
+                </el-radio-group>
                 <el-select
                     v-if="!isNew"
                     class="u-source-id"
@@ -27,30 +27,17 @@
                     @clear="handleClear"
                     @change="handleChange"
                 >
-                    <el-option
-                        v-for="item in options.sources"
-                        :key="item.id"
-                        :label="item.name"
-                        :value="item.id"
-                    >
+                    <el-option v-for="item in options.sources" :key="item.id" :label="item.name" :value="item.id">
                         <div class="u-option">
                             <span class="u-type" v-text="item.label"></span>
                             <span class="u-name" v-text="item.name"></span>
                         </div>
                     </el-option>
                 </el-select>
-                <template v-if="isNew">    
+                <template v-if="isNew">
                     <div class="u-source-top">
-                        <el-select v-if="types"
-                            class="u-source-type"
-                            placeholder="选择通识类型"
-                            v-model="knowledge.type"
-                        >
-                            <el-option v-for="type in types"
-                                :key="type.name"
-                                :value="type.name"
-                                :label="type.label"
-                            ></el-option>
+                        <el-select v-if="types" class="u-source-type" placeholder="选择通识类型" v-model="knowledge.type">
+                            <el-option v-for="type in types" :key="type.name" :value="type.name" :label="type.label"></el-option>
                         </el-select>
                         <el-input class="u-source-name" placeholder="请输入通识名称" v-model="knowledge.name"></el-input>
                     </div>
@@ -62,45 +49,24 @@
                             size="mini"
                             >添加通识</el-button
                         > -->
-                        <span
-                            >Note：添加完成后的通识需要经过管理员审核通过后才会在通识百科上展示哦</span
-                        >
+                        <span>Note：添加完成后的通识需要经过管理员审核通过后才会在通识百科上展示哦</span>
                     </p>
                 </template>
             </div>
 
             <div class="m-publish-remark">
                 <el-divider content-position="left">修订说明 *</el-divider>
-                <el-input
-                    v-model="post.remark"
-                    :maxlength="200"
-                    :minlength="1"
-                    show-word-limit
-                    required
-                    placeholder="请简单描述一下本次修订说明"
-                ></el-input>
+                <el-input v-model="post.remark" :maxlength="200" :minlength="1" show-word-limit required placeholder="请简单描述一下本次修订说明"></el-input>
             </div>
 
             <div class="m-publish-content">
                 <el-divider content-position="left">通识正文 *</el-divider>
-                <Tinymce
-                    v-model="post.content"
-                    :attachmentEnable="true"
-                    :resourceEnable="true"
-                    :height="600"
-                />
+                <Tinymce v-model="post.content" :attachmentEnable="true" :resourceEnable="true" :height="600" />
             </div>
 
             <div class="m-publish-tags">
                 <el-divider content-position="left">通识标签</el-divider>
-                <el-tag
-                    :key="key"
-                    v-for="(tag, key) in post.tags"
-                    closable
-                    :disable-transitions="false"
-                    @close="post.tags.splice(key, 1)"
-                    >{{ tag }}
-                </el-tag>
+                <el-tag :key="key" v-for="(tag, key) in post.tags" closable :disable-transitions="false" @close="post.tags.splice(key, 1)">{{ tag }} </el-tag>
                 <el-input
                     class="input-new-tag"
                     v-if="inputVisible"
@@ -112,68 +78,33 @@
                     @blur="handleInputConfirm"
                 >
                 </el-input>
-                <el-button
-                    v-else
-                    class="button-new-tag"
-                    size="small"
-                    @click="showInput"
-                    >+ 添加标签
-                </el-button>
+                <el-button v-else class="button-new-tag" size="small" @click="showInput">+ 添加标签 </el-button>
             </div>
 
             <div class="m-publish-commit">
                 <el-divider content-position="left"></el-divider>
-                <el-button
-                    class="u-publish"
-                    icon="el-icon-s-promotion"
-                    type="primary"
-                    @click="handleSubmit"
-                    :disabled="processing"
-                    >提交通识
-                </el-button>
+                <el-button class="u-publish" icon="el-icon-s-promotion" type="primary" @click="handleSubmit" :disabled="processing">提交通识 </el-button>
             </div>
         </el-form>
 
         <!-- 通识创建表单 -->
-        <el-dialog
-            title="通识添加"
-            :visible.sync="dialogVisible"
-            width="640px"
-            @open="knowledge = { type: '', name: '' }"
-        >
+        <el-dialog title="通识添加" :visible.sync="dialogVisible" width="640px" @open="knowledge = { type: '', name: '' }">
             <el-form class="m-publish-knowledge" v-if="knowledge">
                 <div class="m-publish-type">
                     <el-divider content-position="left">类别 *</el-divider>
                     <el-radio-group v-if="types" v-model="knowledge.type">
-                        <el-radio
-                            :label="key"
-                            border
-                            v-for="(type, key) in types"
-                            :key="type.name"
-                            >{{ type.label }}
-                        </el-radio>
+                        <el-radio :label="key" border v-for="(type, key) in types" :key="type.name">{{ type.label }} </el-radio>
                     </el-radio-group>
                 </div>
 
                 <div class="m-publish-name">
                     <el-divider content-position="left">名称 *</el-divider>
-                    <el-input
-                        v-model="knowledge.name"
-                        placeholder="请输入通识名称"
-                        :minlength="1"
-                        :maxlength="50"
-                        show-word-limit
-                    ></el-input>
+                    <el-input v-model="knowledge.name" placeholder="请输入通识名称" :minlength="1" :maxlength="50" show-word-limit></el-input>
                 </div>
             </el-form>
             <div slot="footer" class="dialog-footer">
                 <el-button @click="dialogVisible = false">取消</el-button>
-                <el-button
-                    type="primary"
-                    :disabled="processing"
-                    @click="create_knowledge"
-                    >添加通识</el-button
-                >
+                <el-button type="primary" :disabled="processing" @click="create_knowledge">添加通识</el-button>
             </div>
         </el-dialog>
     </div>
@@ -184,10 +115,9 @@ import header from "@/components/publish_header.vue";
 import Tinymce from "@jx3box/jx3box-editor/src/Tinymce";
 
 // 本地依赖
-import {JX3BOX} from "@jx3box/jx3box-common";
-import {WikiPost} from "@jx3box/jx3box-common/js/helper";
+import { WikiPost } from "@jx3box/jx3box-common/js/helper";
 import User from "@jx3box/jx3box-common/js/user";
-import {get_menus, get_list, create_knowledge} from "../service/knowledge";
+import { get_menus, get_list, create_knowledge } from "../service/knowledge";
 import cloneDeep from "lodash/cloneDeep";
 
 export default {
@@ -196,8 +126,8 @@ export default {
         return {
             dialogVisible: false,
             knowledge: {
-                type: '',
-                name: ''
+                type: "",
+                name: "",
             },
             post: {
                 id: "",
@@ -214,45 +144,48 @@ export default {
             inputVisible: false,
             inputValue: "",
 
-            processing : false,
+            processing: false,
 
             // isNew: false, // 是否为新建
             copySource: [],
 
             // 用户动作
-            action: 'new'
+            action: "new",
         };
     },
     computed: {
         isNew() {
-            return this.action === 'new'
-        }
+            return this.action === "new";
+        },
     },
     methods: {
         create_knowledge() {
-            this.processing = true
+            this.processing = true;
             return create_knowledge({
                 type: this.knowledge.type,
                 name: this.knowledge.name,
-            }).then((res) => {
-                res = res.data;
-                if (res.code === 200) {
-                    // 自动选择已添加项
-                    this.post.source_id = res.data.id;
-                    // this.$message({ message: "通识添加成功", type: "success" });
-                    // this.dialogVisible = false;
-                } else {
-                    this.$message({
-                        message: `${res.message}`,
-                        type: "warning",
-                    });
-                }
-            }).finally(()=>{
-                this.processing = false
-            });
+                client : 'all'
+            })
+                .then((res) => {
+                    res = res.data;
+                    if (res.code === 200) {
+                        // 自动选择已添加项
+                        this.post.source_id = res.data.id;
+                        // this.$message({ message: "通识添加成功", type: "success" });
+                        // this.dialogVisible = false;
+                    } else {
+                        this.$message({
+                            message: `${res.message}`,
+                            type: "warning",
+                        });
+                    }
+                })
+                .finally(() => {
+                    this.processing = false;
+                });
         },
         // 通识搜索
-        search_handle(keyword = '') {
+        search_handle(keyword = "") {
             this.options.loading = true;
             get_list({
                 keyword: keyword,
@@ -276,7 +209,7 @@ export default {
 
                 // 重置待选表单
                 if (!keyword) {
-                    this.options.sources = this.copySource
+                    this.options.sources = this.copySource;
                 }
             });
         },
@@ -287,19 +220,19 @@ export default {
             if (!result) this.$message.error("请完成必填项");
             return result;
         },
-        handleSubmit: function (){
+        handleSubmit: function() {
             if (this.isNew) {
                 this.create_knowledge().then(() => {
-                    this.publish()
-                })
+                    this.publish();
+                });
             } else {
-                this.publish()
+                this.publish();
             }
         },
-        publish: function() {            
+        publish: function() {
             // 表单校验
             if (!this.validate()) return;
-            this.processing = true
+            this.processing = true;
             WikiPost.save({
                 type: "knowledge",
                 source_id: this.post.source_id,
@@ -307,19 +240,21 @@ export default {
                 content: this.post.content,
                 remark: this.post.remark,
                 tags: this.post.tags,
-            }).then((res) => {
-                res = res.data;
-                if (res.code === 200) {
-                    this.$message({message: "提交成功，请等待审核", type: "success"});
-                    setTimeout(() => {
-                        this.$router.push({name: 'wiki_post', params: {type: 'knowledge'}})
-                    }, 500);
-                } else {
-                    this.$message({message: `${res.message}`, type: "warning"});
-                }
-            }).finally(()=>{
-                this.processing = false
-            });
+            })
+                .then((res) => {
+                    res = res.data;
+                    if (res.code === 200) {
+                        this.$message({ message: "提交成功，请等待审核", type: "success" });
+                        setTimeout(() => {
+                            this.$router.push({ name: "wiki_post", params: { type: "knowledge" } });
+                        }, 500);
+                    } else {
+                        this.$message({ message: `${res.message}`, type: "warning" });
+                    }
+                })
+                .finally(() => {
+                    this.processing = false;
+                });
         },
         showInput() {
             this.inputVisible = true;
@@ -335,18 +270,17 @@ export default {
             this.inputVisible = false;
             this.inputValue = "";
         },
-        handleChange: function (val) {
-            this.knowledge.name = val
-            this.post.source_id = val
+        handleChange: function(val) {
+            this.knowledge.name = val;
+            this.post.source_id = val;
         },
-        handleClear: function (){
-            console.log(1)
+        handleClear: function() {
             this.options.sources = this.copySource;
-        }
+        },
     },
     created() {
         // 初始化搜索列表
-        this.search_handle('');
+        this.search_handle("");
         // 获取百科ID并通过watch获取攻略
         let id = parseInt(this.$route.params.source_id);
         this.post.source_id = id || "";
@@ -362,52 +296,49 @@ export default {
                 if (!this.post.source_id) {
                     this.post = this.$options.data().post;
                 } else {
-                    WikiPost.newest("knowledge", this.post.source_id, 0).then(
-                        (res) => {
-                            let data = res.data;
-                            if (data.code === 200) {
-                                // 数据填充
-                                let post = data.data.post;
-                                let source = data.data.source;
-    
-                                if (post) {
-                                    this.post.source_id = parseInt(post.source_id);
-                                    this.post.remark = post.remark;
-                                    this.post.content = post.content;
-                                    this.post.tags = post.tags || [];
-                                } else {
-                                    this.post.source_id = this.post.source_id
-                                    this.post.remark = "";
-                                    this.post.content = "";
-                                    this.post.tags = [];
-                                }
-    
-                                if (source) {
-                                    // 将选择项恢复至下拉框
-                                    let exist = false;
-                                    this.options.sources =
-                                        this.options.sources || [];
-                                    for (let index in this.options.sources) {
-                                        if (this.options.sources[index].id == source.id) {
-                                            exist = true;
-                                            break;
-                                        }
+                    WikiPost.newest("knowledge", this.post.source_id, 0).then((res) => {
+                        let data = res.data;
+                        if (data.code === 200) {
+                            // 数据填充
+                            let post = data.data.post;
+                            let source = data.data.source;
+
+                            if (post) {
+                                this.post.source_id = parseInt(post.source_id);
+                                this.post.remark = post.remark;
+                                this.post.content = post.content;
+                                this.post.tags = post.tags || [];
+                            } else {
+                                this.post.source_id = this.post.source_id;
+                                this.post.remark = "";
+                                this.post.content = "";
+                                this.post.tags = [];
+                            }
+
+                            if (source) {
+                                // 将选择项恢复至下拉框
+                                let exist = false;
+                                this.options.sources = this.options.sources || [];
+                                for (let index in this.options.sources) {
+                                    if (this.options.sources[index].id == source.id) {
+                                        exist = true;
+                                        break;
                                     }
-                                    if (!exist) this.options.sources.push(source);
                                 }
+                                if (!exist) this.options.sources.push(source);
                             }
                         }
-                    );
+                    });
                 }
             },
         },
-        action: function (){
+        action: function() {
             this.knowledge = this.$options.data().knowledge;
-            this.post = this.$options.data().post
-        }
+            this.post = this.$options.data().post;
+        },
     },
     components: {
-        'publish-header':header,
+        "publish-header": header,
         Tinymce,
     },
 };
