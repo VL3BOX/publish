de7ef00f05049ee7403ecf136d7af0875ddd6625
Kaviilee <jiaweilee95@126.com>
2022-05-16 23:30:43 +0800

    update: upload component

--- src/components/publish_banner.vue
+++ src/components/publish_banner.vue
@@ -1,37 +1,26 @@
 <template>
     <div class="m-publish-banner">
         <el-divider content-position="left">海报</el-divider>
-        <el-alert class="u-tip" title="首页海报尺寸1100*300，非必选，部分栏目不会展示海报，分享功能将使用该图作为预览图" type="info" show-icon></el-alert>
-        <el-upload
-            class="avatar-uploader"
-            :action="url"
-            :show-file-list="false"
-            :on-success="done"
-            :on-error="fail"
-            with-credentials
-            accept="image/jpg,image/jpeg,image/gif,image/png,image/bmp"
-        >
-            <img v-if="banner" :src="banner | showBanner" />
-            <i v-else class="el-icon-plus avatar-uploader-icon"></i>
-        </el-upload>
-        <el-button class="u-remove" type="info" size="mini" icon="el-icon-circle-close" @click="clearBanner">移除海报</el-button>
+        <uploadImage v-model="banner" info="首页海报尺寸1100*300，非必选，部分栏目不会展示海报，分享功能将使用该图作为预览图"></uploadImage>
     </div>
 </template>
 
 <script>
-import axios from "axios";
 import { __cms } from "@jx3box/jx3box-common/data/jx3box.json";
 const API_Root = process.env.NODE_ENV === "production" ? __cms : "/";
 const API = API_Root + "api/cms/upload";
 import { showBanner } from "@jx3box/jx3box-common/js/utils";
+import uploadImage from '@jx3box/jx3box-common-ui/src/upload/upload_banner.vue'
 
 export default {
     name: "post_banner",
+    components: {
+        uploadImage
+    },
     props: ["data"],
     data() {
         return {
             banner: this.data,
-            url: API,
         };
     },
     model: {
@@ -46,25 +35,6 @@ export default {
             this.$emit("update", newval);
         },
     },
-    methods: {
-        done(res, file) {
-            this.banner = res.data[0];
-        },
-        fail(err, file, fileList) {
-            try {
-                let response = JSON.parse(err.message);
-                this.$message.error(`[${response.code}]${response.msg}`);
-            } catch {
-                this.$message.error("网络请求异常");
-            }
-        },
-        clearBanner : function (){
-            this.banner = ''
-        }
-    },
-    filters : {
-        showBanner
-    }
 };
 </script>
 
