93eff9dbd1b8d660078899f556d391e6516e6e4c
Kaviilee <jiaweilee95@126.com>
2022-09-08 15:40:08 +0800

    fix: 移除js中判断文件格式

--- src/components/face_attachment.vue
+++ src/components/face_attachment.vue
@@ -37,11 +37,6 @@ export default {
         uploadData(e) {
             let formdata = new FormData();
             let file = e.target.files[0];
-            // 数据类型必须为 .jx3dat .dat
-            if (!file.name.endsWith('.jx3dat') && !file.name.endsWith('.dat')) {
-                this.$message.error('文件格式错误');
-                return;
-            }
 
             formdata.append("jx3dat", file, "data.jx3dat");
             formdata.append("body", this.body);
