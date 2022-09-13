e43031d0e678e68cdf2024e7af8acf787455e97c
whc2001 <ianwang0122@outlook.com>
2022-09-13 11:16:07 -0400

    修正发布页面发布按钮路径

--- src/paid/face.vue
+++ src/paid/face.vue
@@ -95,7 +95,7 @@ export default {
             };
         },
         publishLink: function () {
-            return "./#/face";
+            return "./publish/#/face";
         },
     },
     watch: {
