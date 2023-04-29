55693c4cdc0b2ee167d84fd71013a3b591c5daf6
Kaviilee <jiaweilee95@126.com>
2023-04-29 22:36:06 +0800

    chore: update cdn

--- src/service/raw.js
+++ src/service/raw.js
@@ -2,7 +2,7 @@ import { $node, axios } from "@jx3box/jx3box-common/js/https";
 import JX3BOX from "@jx3box/jx3box-common/data/jx3box.json";
 
 const $oss = axios.create({
-    baseURL: JX3BOX.__ossRoot,
+    baseURL: JX3BOX.__dataPath,
 });
 
 function getSkills(params) {
@@ -12,12 +12,12 @@ function getSkills(params) {
 }
 
 async function getTalents() {
-    let res = await $oss.get("/data/qixue/talents.json");
+    let res = await $oss.get("talent/talents.json");
     return res.data;
 }
 
 async function getTalents2() {
-    let res = await $oss.get("/data/talent2/talent2.json");
+    let res = await $oss.get("talent2/talent2.json");
     return res.data;
 }
 
