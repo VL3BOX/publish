871665c1735642f835bc41a22c2c9f53bcd7cc5b
Kaviilee <jiaweilee95@126.com>
2023-06-04 21:43:50 +0800

    chore: update item plan

--- src/service/item_plan.js
+++ src/service/item_plan.js
@@ -39,29 +39,29 @@ function delete_item_plan(plan_id) {
 // 获取我的清单
 function getMyPlans(params) {
 	return $cms()
-		.get(`/api/cms/app/item_plans/mine`, { params })
+		.get(`/api/cms/app/item-plan/mine`, { params })
 		.then((res) => {
 			return res.data.data;
 		});
 }
 function getPlanByID(id) {
 	return $cms()
-		.get(`/api/cms/app/item_plan/${id}`)
+		.get(`/api/cms/app/item-plan/${id}`)
 		.then((res) => {
 			return res.data.data;
 		});
 }
 // 新增清单
 function addMyPlans(params) {
-	return $cms().post(`/api/cms/app/item_plan`, params);
+	return $cms().post(`/api/cms/app/item-plan`, params);
 }
 // 修改我的清单
-function postMyPlans(id, params) { 
-	return $cms().put(`/api/cms/app/item_plan/${id}`, params);
+function postMyPlans(id, params) {
+	return $cms().put(`/api/cms/app/item-plan/${id}`, params);
 }
 // 删除我的清单
 function delMyPlans(id) {
-	return $cms().delete(`/api/cms/app/item_plan/${id}`);
+	return $cms().delete(`/api/cms/app/item-plan/${id}`);
 }
 
 export { get_item_plan, save_item_plan, get_my_item_plans, delete_item_plan, getMyPlans, delMyPlans, addMyPlans, postMyPlans, getPlanByID };
